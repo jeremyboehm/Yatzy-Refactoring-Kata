a42fa1bc7c32f638143a358eadfbca36a9be6c34
Jérémy Boehm <jeremyboehm77@gmail.com>
2021-03-27 09:55:30 +0100

    Refactoring twos test method + ones method

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -19,12 +19,10 @@ public class Yatzy {
     }
 
     public static int ones(List<Integer> rolls) {
-        return (int) rolls.stream().filter(roll -> roll.equals(1)).count();
+        return (int) rolls.stream().filter(roll -> roll.equals(1)).mapToLong(roll -> roll).sum();
     }
 
-    public static int twos(int d1, int d2, int d3, int d4, int d5) {
-        List<Integer> rolls = Arrays.asList(d1, d2, d3, d4, d5);
-
+    public static int twos(List<Integer> rolls) {
         return (int) rolls.stream().filter(roll -> roll.equals(2)).mapToLong(roll -> roll).sum();
     }
 
