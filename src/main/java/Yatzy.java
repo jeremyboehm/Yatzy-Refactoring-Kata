c94716f9441d578515d7d8cce1b17a8ffcdecc40
Jérémy Boehm <jeremyboehm77@gmail.com>
2021-03-28 13:30:13 +0200

    Refactoring full house method

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -82,37 +82,13 @@ public class Yatzy {
     }
 
     public static int fullHouse(int d1, int d2, int d3, int d4, int d5) {
-        int[] tallies;
-        boolean _2 = false;
-        int i;
-        int _2_at = 0;
-        boolean _3 = false;
-        int _3_at = 0;
+        List<Integer> rolls = asList(d1, d2, d3, d4, d5);
 
+        int twoOfAKind = numberOfAKind(rolls, 2);
+        int threeOfAKind = numberOfAKind(rolls, 3);
 
-        tallies = new int[6];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
+        return twoOfAKind == 0 || threeOfAKind == 0 ? 0 : twoOfAKind + threeOfAKind;
 
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 2) {
-                _2 = true;
-                _2_at = i + 1;
-            }
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 3) {
-                _3 = true;
-                _3_at = i + 1;
-            }
-
-        if (_2 && _3)
-            return _2_at * 2 + _3_at * 3;
-        else
-            return 0;
     }
 
     private int getSumOf(List<Integer> rolls, int number) {
@@ -167,6 +143,18 @@ public class Yatzy {
 
             if (frequency >= kind) {
                 return roll*kind;
+            }
+        }
+        return 0;
+    }
+
+    private static int numberOfAKind(List<Integer> rolls, int number) {
+        int frequency;
+        for (Integer roll : rolls) {
+            frequency = Collections.frequency(rolls, roll);
+
+            if (frequency >= number) {
+                return roll*number;
             }
         }
         return 0;
